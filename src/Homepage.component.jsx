bfe892a56fa6d837f41168d51220834e5ed39db4
anassdeveloper <chnetoufanass28@gmail.com>
2023-02-09 21:33:45 +0100

    start project

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Homepage.style.scss";
+
+
+const Homepage = () => {
+    return (
+        <div className="heading">HomePage</div>
+    )
+}
+export default Homepage;
